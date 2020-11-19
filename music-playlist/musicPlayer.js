399005ecd96cc590c12dbad2bcf20ec81b467c60
soon <69294741+vlyly@users.noreply.github.com>
2020-11-19 13:43:32 +0900

    음악 플레이 리스트02
    -popup player 세로 정렬 방식 변경
    -text-slide artistShowMore 동시에 사용 가능
    
    *showMoreButton toggle 수정할 것

--- music-playlist/musicPlayer.js
+++ music-playlist/musicPlayer.js
@@ -3,16 +3,8 @@ const musicItem = document.querySelectorAll(".musicItem");
 const musicPlayerLayer_popup = document.querySelector(
   ".musicPlayer-popup-container"
 );
-const musicDetailhaha = document.querySelector(".musicDetail");
 const quitButton = document.querySelector(".musicPlayer-quitButton");
-
-function closePlayer() {
-  while (musicPlayerLayer_popup.childNodes.length > 2) {
-    musicPlayerLayer_popup.removeChild(musicPlayerLayer_popup.lastChild);
-  }
-
-  musicPlayerLayer.style.display = "";
-}
+const showMoreButton = document.createElement("button");
 
 function clickMusicItem() {
   const musicInfo = this.id;
@@ -22,7 +14,25 @@ function clickMusicItem() {
   const musicTitle = musicDetail.firstElementChild;
   const artist = musicDetail.lastElementChild;
 
-  if (musicTitle.innerText.length > 30) {
+  albumImage.src = `./trial02-musicPlayList/${musicInfo}.jpg`;
+  albumImage.alt = `${musicInfo} 앨범아트`;
+  albumImage.classList.add("musicPlayer-albumImage");
+
+  musicDetail.classList.add("align-items");
+  musicDetail.classList.add("musicPlayer-musicDetail");
+
+  playerMusic.src = `./trial02-musicPlayList/${musicInfo}.mp3`;
+  playerMusic.type = "audio/mpeg";
+  playerMusic.controls = true;
+  playerMusic.classList.add("musicPlayer-controller");
+
+  musicPlayerLayer_popup.appendChild(albumImage);
+  musicPlayerLayer_popup.appendChild(musicDetail);
+  musicPlayerLayer_popup.appendChild(playerMusic);
+
+  musicPlayerLayer.style.display = "flex";
+
+  if (musicTitle.scrollWidth > 190) {
     const textSlide_container = document.createElement("div");
     const musicTitle_copy = musicTitle.cloneNode(true);
 
@@ -34,26 +44,32 @@ function clickMusicItem() {
     musicDetail.classList.add("longTitle");
   }
 
-  albumImage.src = `./trial02-musicPlayList/${musicInfo}.jpg`;
-  albumImage.alt = `${musicInfo} 앨범아트`;
-  albumImage.classList.add("musicPlayer-albumImage");
+  if (artist.scrollWidth > 190) {
+    showMoreButton.classList.add("showMoreButton");
 
-  musicDetail.classList.add("align-items");
+    musicDetail.appendChild(showMoreButton);
+  }
 
-  playerMusic.src = `./trial02-musicPlayList/${musicInfo}.mp3`;
-  playerMusic.type = "audio/mpeg";
-  playerMusic.controls = true;
-  playerMusic.classList.add("musciPlayer-controller");
+  function artist_showMore() {
+    artist.classList.toggle("showMoreArtist");
+    showMoreButton.classList.toggle("showMoreButton_close");
+  }
 
-  musicPlayerLayer_popup.appendChild(albumImage);
-  musicPlayerLayer_popup.appendChild(musicDetail);
-  musicPlayerLayer_popup.appendChild(playerMusic);
+  function closePlayer() {
+    while (musicPlayerLayer_popup.childNodes.length > 2) {
+      musicPlayerLayer_popup.removeChild(musicPlayerLayer_popup.lastChild);
+    }
+    musicPlayerLayer.style.display = "";
+  }
 
-  musicPlayerLayer.style.display = "flex";
+  showMoreButton.addEventListener("click", artist_showMore);
+  quitButton.addEventListener("click", closePlayer);
 }
 
-quitButton.addEventListener("click", closePlayer);
+function init() {
+  Array.from(musicItem).forEach((item) =>
+    item.addEventListener("click", clickMusicItem)
+  );
+}
 
-Array.from(musicItem).forEach((item) =>
-  item.addEventListener("click", clickMusicItem)
-);
+init();
